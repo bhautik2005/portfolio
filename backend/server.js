f79c9dcdbd28f6789ac5ce519b9701d5f1990c29
bhautik2005 <gondaliyabhautik419@gmail.com>
2026-04-08 07:27:38 +0530

    add cors url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://bhautik2005.pages.dev/", // your Cloudflare URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB connection
