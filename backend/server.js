75a41ce2633b398d512a95372fa2d4d2d0fefbaf
bhautik2005 <gondaliyabhautik419@gmail.com>
2026-04-08 07:36:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: "https://bhautik2005.pages.dev/", // your Cloudflare URL
+  origin: "https://bhautik2005.pages.dev", // your Cloudflare URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
