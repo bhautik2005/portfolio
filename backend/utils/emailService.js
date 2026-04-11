9b1ff6f07535215775149438167fa0d708658ec7
bhautik2005 <gondaliyabhautik419@gmail.com>
2026-04-11 08:40:12 +0530

    fix smtp port 587

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -3,7 +3,9 @@ import nodemailer from 'nodemailer';
 const sendContactEmail = async ({ name, email, subject, message }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
